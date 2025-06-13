2b747e4ac3b49b0bd8329af6ccb527ce576e4633
Egov <fahrurrozy4214@gmail.com>
2025-06-14 02:55:25 +0700

    feat : api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,41 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserRequestController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Auth routes
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::put('/profile', [AuthController::class, 'updateProfile']);
+    Route::post('/change-password', [AuthController::class, 'changePassword']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // User Request routes
+    Route::prefix('requests')->group(function () {
+        Route::get('/', [UserRequestController::class, 'index']);
+        Route::get('/statistics', [UserRequestController::class, 'statistics']);
+        Route::post('/', [UserRequestController::class, 'store']);
+        Route::get('/{id}', [UserRequestController::class, 'show'])->middleware('user.owns.request');
+    });
+});

02e5fedec7dbc5725a294bb622b9285f54fa1ad1
fhrrrzy <fahrurrozy4214@gmail.com>
2025-07-03 07:16:47 +0700

    fix : remove lampiran col

--- app/Filament/Resources/UserRequestResource.php
+++ app/Filament/Resources/UserRequestResource.php
@@ -141,6 +141,7 @@ class UserRequestResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->defaultSort('created_at', 'desc')
             ->columns([
                 Tables\Columns\TextColumn::make('title')
                     ->label('Judul')
